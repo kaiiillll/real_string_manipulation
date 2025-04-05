62ea93dba37c852cad86825eea6b320e3bf6636a
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-05 22:55:37 +0800

    reverse_casing

--- reverse_casing.py
+++ reverse_casing.py
@@ -1,2 +1,3 @@
-user_input = input("Enter your name: ").swapcase()
-print(user_input)
+# ask the user to input name
+user_input = input("Enter your name: ").swapcase() # use the .swapcase to reverse the capitalization
+print(user_input) # print the results
