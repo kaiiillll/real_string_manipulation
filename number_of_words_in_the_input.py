c284e20b2a7ea6ab3c392105bffdd6ac811ca68e
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-05 23:02:39 +0800

    number_of_words_in_the_input

--- number_of_words_in_the_input.py
+++ number_of_words_in_the_input.py
@@ -1,3 +1,5 @@
 # ask the user to input sentence
-# use len and split manipulation to count
-# print the results
+user_input = input("Enter your name: ") 
+number_of_words = len(user_input.split()) # use len and split manipulation to count
+print(number_of_words) # print the results
+
